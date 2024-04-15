70db3f4bacca7a15326ed47d9e122b1fe50d71ff
Adrial Natanael Liong <adrial2_nathanael@yahoo.com>
2024-04-15 18:03:24 +0700

    Create Notification model struct.”

---
+++ src/model/subscriber.rs
@@ -0,0 +1,12 @@
+use rocket::serde::{Deserialize, Serialize}; use rocket::Log;
+use rocket::log;
+use rocket::serde::json::to_string;
+use rocket::tokio;
+use bambangshop::REQWEST_CLIENT;
+use crate::model::notification:: Notification;
+#[derive(Debug, Clone, Deserialize, Serialize)]
+#[serde(crate = "rocket::serde")]
+pub struct Subscriber {
+    pub url: String,
+    pub name: String,
+}
